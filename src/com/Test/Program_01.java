b65ca6fc7f4176db66552f2c08cb43777d76872a
Sinchnaaa <sinchd9686@gmail.com>
2025-05-06 09:40:11 +0530

    Update Program_01.java

--- src/com/Test/Program_01.java
+++ src/com/Test/Program_01.java
@@ -4,7 +4,10 @@ public class Program_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Hello Word");
+		System.out.print("Enter a number: ");
+		int n=s.nextInt();
+		System.out.println("Square of "+ n +" is :" +(n*n));
 	}
 
 }
